430d34d29ac720d1c8acb33f5f60175fea1c3346
Yassel Avila <yassel.avila@gmail.com>
2018-12-08 08:49:40 -0500

    Base components and Shared module import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { UserDataComponent } from './user-data/user-data.component';
+import { UsersListComponent } from './users-list/users-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, SharedModule],
   providers: [],
+  declarations: [AppComponent, UsersListComponent, UserDataComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
