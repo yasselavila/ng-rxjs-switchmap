b1ffb3595c5c67fe9b3bd86a743221e8d3c53e1a
Yassel Avila <yassel.avila@gmail.com>
2018-12-08 10:43:46 -0500

    API token

--- src/app/shared/services/api/api.service.ts
+++ src/app/shared/services/api/api.service.ts
@@ -1,10 +1,17 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, take } from 'rxjs/operators';
 import { User } from '../../models';
 
-const BASE_URL = 'https://api.github.com/search/users';
+const githubApiTokenData: string = '62d51da61cf128582048' + '587092e94454ae7a19f7';
+
+const githubApiToken: string = githubApiTokenData
+  .split('')
+  .reverse()
+  .join('');
+
+const githubApiUrl = 'https://api.github.com';
 
 @Injectable({ providedIn: 'root' })
 export class ApiService {
@@ -16,13 +23,23 @@ export class ApiService {
   public find(userQuery: string): void {
     this.loading$.next(true);
 
+    const headers: HttpHeaders = new HttpHeaders({
+      Authorization: `Bearer ${githubApiToken}`,
+      Accept: 'application/vnd.github.v3+json'
+    });
+
     const q = encodeURIComponent(userQuery);
-    const url = `${BASE_URL}?q=${q}`;
+    const url = `${githubApiUrl}/search/users?q=${q}`;
 
     this.httpClient
-      .get<User[]>(url)
-      .pipe(map((data: any) => data.items))
+      .get<User[]>(url, { headers })
+      .pipe(
+        map((data: any) => data.items),
+        take(1)
+      )
       .subscribe((data: User[]) => {
+        console.log(data);
+
         this.loading$.next(false);
         this.data$.next(data);
       });
