6033afcb28cb91bba8ab78a67d673c03d31958c9
Yassel Avila <yassel.avila@gmail.com>
2018-12-08 09:57:03 -0500

    Spinner

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -1,7 +1,12 @@
 import { ModuleWithProviders } from '@angular/core';
 import { NotFoundComponent } from './not-found';
 import { SearchBarComponent } from './search-bar/search-bar.component';
+import { SpinnerComponent } from './spinner/spinner.component';
 
-export { NotFoundComponent, SearchBarComponent };
+export { NotFoundComponent, SearchBarComponent, SpinnerComponent };
 
-export const COMPONENTS: Array<ModuleWithProviders | any> = [NotFoundComponent, SearchBarComponent];
+export const COMPONENTS: Array<ModuleWithProviders | any> = [
+  NotFoundComponent,
+  SearchBarComponent,
+  SpinnerComponent
+];
---
+++ src/app/shared/components/spinner/spinner.component.spec.ts
@@ -0,0 +1,26 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { SpinnerComponent } from './spinner.component';
+
+describe('[Shared] Component: Spinner', () => {
+  let fixture: ComponentFixture<SpinnerComponent>;
+  let cmp: SpinnerComponent;
+  let element: any;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [SpinnerComponent]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SpinnerComponent);
+    cmp = fixture.componentInstance;
+    element = fixture.nativeElement;
+
+    fixture.detectChanges();
+  });
+
+  it('should create the component', () => {
+    expect(cmp).toBeTruthy();
+  });
+});
