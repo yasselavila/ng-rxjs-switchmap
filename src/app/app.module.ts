7dca0fb4262b4fcce0957827cc961b533a1f1c57
Yassel Avila <yassel.avila@gmail.com>
2018-12-08 08:56:16 -0500

    Search bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { UserDataComponent } from './user-data/user-data.component';
 import { UsersListComponent } from './users-list/users-list.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
   imports: [BrowserModule, AppRoutingModule, SharedModule],
   providers: [],
-  declarations: [AppComponent, UsersListComponent, UserDataComponent],
+  declarations: [AppComponent, UsersListComponent, UserDataComponent, SearchBarComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
