63b8219d2a5fe0d39bd4cb7075ab67a8a932fcd8
Yassel Avila <yassel.avila@gmail.com>
2018-12-08 09:50:11 -0500

    Search bar

--- src/app/shared/components/search-bar/search-bar.component.ts
+++ src/app/shared/components/search-bar/search-bar.component.ts
@@ -1,12 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.scss']
+  styleUrls: ['./search-bar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SearchBarComponent implements OnInit {
-  constructor() {}
+  private _currentValue = '';
 
-  ngOnInit() {}
+  public formControl: FormControl = new FormControl('');
+
+  @Output()
+  public change: EventEmitter<string> = new EventEmitter<string>();
+
+  public ngOnInit(): void {
+    this.formControl.valueChanges.pipe(debounceTime(500)).subscribe((value: string) => {
+      if (this._currentValue !== value) {
+        this.change.emit(value);
+        this._currentValue = value;
+      }
+    });
+  }
 }
