ba1f2a23c214a83b29580768db94509f37552090
Yassel Avila <yassel.avila@gmail.com>
2018-12-08 08:56:29 -0500

    Display user data

--- src/app/user-data/user-data.component.ts
+++ src/app/user-data/user-data.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { User } from '../shared/models';
 
 @Component({
   selector: 'app-user-data',
   templateUrl: './user-data.component.html',
   styleUrls: ['./user-data.component.scss']
 })
-export class UserDataComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class UserDataComponent {
+  @Input()
+  user: User;
 }
